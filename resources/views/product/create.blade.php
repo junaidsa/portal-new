9658df04c4e15f0a52af7cb186b2b4b499fe85d2
junaidsa <94611843+junaidsa@users.noreply.github.com>
2024-10-01 21:11:02 -0700

    done

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('main')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Categories/</span>Create</h4>
+        <div class="row">
+            <!-- Form controls -->
+            <div class="col-md-12">
+                <div class="card mb-4">
+                    <h5 class="card-header">Create Category</h5>
+                    <div class="card-body">
+                        <form action="{{ url('category/store') }}" method="POST">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="subject_name" class="form-label">Name</label>
+                                        <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                            id="name" name="name" placeholder="Enter  Name" />
+                                        @error('name')
+                                            <div class=" invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label for="exampleFormControlSelect1" class="form-label">Subject Status</label>
+                                        <select class="form-select" id="status" name="status"
+                                            aria-label="Default select example">
+                                            <option value="1">Active</option>
+                                            <option value="0">Deactive</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4 mt-3"><button class="btn btn-primary d-grid w-50">Submit</button></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
