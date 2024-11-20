d651648bb9513dfb389c311fda7ffd15a4bdfd42
junaidsa <94611843+junaidsa@users.noreply.github.com>
2024-11-20 23:28:17 +0500

    done

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Schedule;
 use App\Models\ScheduleTiming;
 use App\Models\Subjects;
 use App\Models\User;
@@ -17,6 +18,11 @@ class ScheduleController extends Controller
        $teacher = User::class::with('branch')->where('role','teacher')->get();
        $subject = Subjects::get();
         return view('student.schedule',compact('schedules','teacher','subject'));
+    }
+    public function bankPayment()
+    {
+            $schedule = Schedule::where('payment_type','Bank')->orderBy('id', 'Desc')->get();
+            return view("bankpayment.index", compact('schedule'));
     }
     
 }
